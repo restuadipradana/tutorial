4ada12b0f4c3f4a5e0f67a17daf5a22ced785d52
Restu Adi Pradana <restu.maduki@live.com>
2019-05-20 22:43:37 +0700

    Create LCD_OLED_Basic.ino

---
+++ LCD_OLED_Basic.ino
@@ -0,0 +1,152 @@
+/**************************************************************************
+ This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+ Pick one up today in the adafruit shop!
+ ------> http://www.adafruit.com/category/63_98
+
+ This example is for a 128x64 pixel display using SPI to communicate
+ 4 or 5 pins are required to interface.
+
+ Adafruit invests time and resources providing this open
+ source code, please support Adafruit and open-source
+ hardware by purchasing products from Adafruit!
+
+ Written by Limor Fried/Ladyada for Adafruit Industries,
+ with contributions from the open source community.
+ BSD license, check license.txt for more information
+ All text above, and the splash screen below must be
+ included in any redistribution.
+ **************************************************************************/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for SSD1306 display connected using software SPI (default case):
+#define OLED_MOSI   23
+#define OLED_CLK   18
+#define OLED_DC    4
+#define OLED_CS    5
+#define OLED_RESET 15
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
+  OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+
+void setup() {
+  Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, WHITE);
+
+
+  display.display();
+  delay(2000);
+ 
+}
+
+void loop() {
+
+  display.clearDisplay();
+
+  // Display Text
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,28);
+  display.println("Hello world!");
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // Display Inverted Text
+  display.setTextColor(BLACK, WHITE); // 'inverted' text
+  display.setCursor(0,28);
+  display.println("Hello world!");
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // Changing Font Size
+  display.setTextColor(WHITE);
+  display.setCursor(0,24);
+  display.setTextSize(2);
+  display.println("Hello!");
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // Display Numbers
+  display.setTextSize(1);
+  display.setCursor(0,28);
+  display.println(123456789);
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // Specifying Base For Numbers
+  display.setCursor(0,28);
+  display.print("0x"); display.print(0xFF, HEX); 
+  display.print("(HEX) = ");
+  display.print(0xFF, DEC);
+  display.println("(DEC)"); 
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // Display ASCII Characters
+  display.setCursor(0,24);
+  display.setTextSize(2);
+  display.write(3);
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // Scroll full screen
+  display.setCursor(0,0);
+  display.setTextSize(1);
+  display.println("Full");
+  display.println("screen");
+  display.println("scrolling!");
+  display.display();
+  display.startscrollright(0x00, 0x07);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+  display.startscrollleft(0x00, 0x07);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);    
+  display.startscrolldiagright(0x00, 0x07);
+  delay(2000);
+  display.startscrolldiagleft(0x00, 0x07);
+  delay(2000);
+  display.stopscroll();
+  display.clearDisplay();
+
+  // Scroll part of the screen
+  display.setCursor(0,0);
+  display.setTextSize(1);
+  display.println("Scroll");
+  display.println("some part");
+  display.println("of the screen.");
+  display.display();
+  display.startscrollright(0x00, 0x00);
+}
